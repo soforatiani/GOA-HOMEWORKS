71e3fe49ef7fd43f1abe24aa4f5f756e036db3db
soforatiani <ratianisopo88@gmail.com>
2025-09-11 22:33:41 +0400

    commit

---
+++ day97/homework/script2.js
@@ -0,0 +1,16 @@
+var name="sofo"
+var surname="ratiani"
+var age="25"
+var height="171"
+
+
+let number=10
+let weight=50
+let volume =50
+let animal="cat"
+
+
+const name="sofo"
+const adress="tbilisi"
+const surname="ratiani"
+const favoriteColor="white"
